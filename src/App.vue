b9b963f392ef86224fc77089ab2276072b574531
karina A <karina.arevalo@hotmail.com>
2022-10-12 19:32:31 -0500

    main page layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <script setup></script>
 
 <template>
-  <RouterView />
+  <main>
+    <div class="card">
+      <RouterView />
+    </div>
+  </main>
 </template>
+
+<style lang="postcss" scoped>
+main {
+  @apply flex h-screen  items-center  justify-center bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400;
+
+  & .card {
+    @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
+  }
+}
+</style>
