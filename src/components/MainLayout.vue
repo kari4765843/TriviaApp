b858586a5ed658f1aaa2106d49206b6cb475651e
karina A <karina.arevalo@hotmail.com>
2022-10-14 12:04:24 -0500

    refactor a little bit

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,21 @@
+<script setup>
+import MainScore from '@/components/MainScore.vue'
+</script>
+
+<template>
+  <main>
+    <div class="card">
+      <RouterView />
+    </div>
+  </main>
+  <MainScore />
+</template>
+
+<style lang="postcss" scoped>
+main {
+  @apply flex h-screen  items-center  justify-center bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400;
+  & .card {
+    @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
+  }
+}
+</style>
