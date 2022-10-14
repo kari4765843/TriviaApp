82eaf08e2912689b584404be20e34bd531bb49a0
karina A <karina.arevalo@hotmail.com>
2022-10-14 13:41:06 -0500

    added base title

---
+++ src/components/BaseTitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="brand">
+    <slot name="logo" />
+    <h1 class="title">
+      <slot />
+    </h1>
+    <slot name="logo" />
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.brand {
+  @apply flex items-center justify-center gap-4;
+
+  & :slotted(img) {
+    @apply h-16 w-16;
+  }
+  & .title {
+    @apply text-6xl  font-extralight text-pink-500;
+  }
+}
+</style>
