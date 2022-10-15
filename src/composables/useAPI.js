f81f09c8162023abb692b178c256a4839bd9ef23
karina A <karina.arevalo@hotmail.com>
2022-10-15 14:12:11 -0500

    added questions

--- src/composables/useAPI.js
+++ src/composables/useAPI.js
@@ -1,13 +1,27 @@
 import axios from 'axios'
-
+import { ref } from 'vue'
 const instance = axios.create({
   baseURL: 'https://opentdb.com/',
 })
 
+const categories = ref([])
+
 export default function useAPI() {
   const getCategories = async () => {
-    const response = await instance.get('api_category.php')
-    return response.data.trivia_categories
+    if (categories.value.length === 0) {
+      const response = await instance.get('api_category.php')
+      return response.data.trivia_categories
+    }
   }
-  return { instance, getCategories }
+
+  const getQuestion = async (categoryId) => {
+    const response = await instance.get('api.php', {
+      params: {
+        amount: 1,
+        category: categoryId,
+      },
+    })
+    return response.data.results[0]
+  }
+  return { instance, categories, getCategories, getQuestion }
 }
